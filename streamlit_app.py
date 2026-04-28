53a8cc71ec93b19b2785e801ed4cb4339d99f0cb
Amy Keith <amyakeith@gmail.com>
2026-04-28 15:22:07 -0500

    Fix smoothiefroot request

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,6 @@ if ingredients_list:
         st.success('Your smoothie is ordered!', icon="✅")
 
 import requests  
-smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
 st.text(smoothiefroot_response.json())
     
