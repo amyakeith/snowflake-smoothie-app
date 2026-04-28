ac135e6c561daa2ca5602c2bcf2e235bf5834f3a
Amy Keith <amyakeith@gmail.com>
2026-04-28 15:40:35 -0500

    Retrieve fruit info using SEARCH_ON value

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,8 +16,6 @@ session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 
 pd_df = my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
 
 name_on_order = st.text_input('Name on Order', '')
 st.write('The name on your smoothie will be: ', name_on_order)
@@ -33,7 +31,8 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
         st.subheader(fruit_chosen + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)  
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)  
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)   
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
